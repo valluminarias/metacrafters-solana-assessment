396696ab618ad960675f5458ada3c1b73b67aea3
Val Luminarias <vluminarias@yahoo.com>
2022-11-16 18:49:00 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    My Solana App
+  </header>
+
+  <main>
+    <button>
+      Create a new Solana account
+    </button>
+
+    <button>
+      Connect to Phantom Wallet
+    </button>
+
+    <button>
+      Transfer to new Wallet
+    </button>
+
+    <div>
+      <p>
+        No provider found. Install
+        <a href="https://phantom.app/">Phantom Browser extension</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
